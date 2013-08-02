b962e69a27bc3c4e4ec82e284e556beffd588d48
Bernie <bernie.wangbj@gmail.com>
2013-08-02 16:52:05 +0800

    add new code to cookie.js json.js

---
+++ src/cookie.js
@@ -0,0 +1,46 @@
+	//Reference: https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
+	rootFore.cookie = {
+		get: function ( name ) {
+			var cookieRegExp = new RegExp( '(?:(?:^|.*;)\\s*' + escape( name ).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=\\s*([^;]*).*$)|^.*$');
+			return unescape( document.cookie.replace( cookieRegExp, '$1' ) ) || null;
+		},
+		set: function ( name, value, option ) {
+			if ( name ) {
+				var cookieStr = name + '=' + escape( value );
+
+				if ( option ) {
+					for ( var oKey in option ) {
+						switch ( oKey ) {
+							case 'duration':
+								var durationNum = parseInt( option[ oKey ] ); // in seconds
+
+								if ( !isNaN( durationNum) ) {
+									var d = new Date();
+									d.setTime( d.getTime() + durationNum * 1000 ); // in milliseconds
+									/*
+									 * expires is old standard supported in all browser,
+									 * max-age is new one in HTTP 1.1 supported in morden browser and >= IE9, 
+									 * every browser that supports max-age will ignore the expires regardless of it’s value.
+									 */
+									cookieStr += '; expires=' + d.toGMTString() + '; max-age=' + durationNum; 
+								}
+								
+								break;
+							case 'path':
+								cookieStr += '; path=' + option[ oKey ];
+								break;
+							case 'domain':
+								cookieStr += '; domain=' + option[ oKey ];
+							case 'secure':
+								cookieStr += '; secure=' + option[ oKey ];
+						}
+					}
+				}
+
+				document.cookie = cookieStr;
+			}
+		},
+		remove: function ( name ) {
+			// body...
+		}
+	};
--- src/json.js
+++ src/json.js
@@ -1,8 +1,11 @@
-	// rootFore.apply( rootFore, {
-	// 	parseJson: function (str) {
-
-	// 	},
-	// 	stringifyJson
-	// } );
-	//TODO
-	// decide to use namespace
+	/*
+	 * using third-party lib json2.js https://github.com/douglascrockford/JSON-js , which is under ../lib/JSON-js-master
+	 */
+	rootFore.json = {
+		parse: function ( str ) {
+			return JSON.parse( str );
+		},
+		stringify: function ( jsonObj ) {
+			return JSON.stringify( jsonObj );
+		}
+	};
