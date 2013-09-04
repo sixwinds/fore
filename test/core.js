16e3174f677efc568213fbd15c10c17e8ba49837
Bernie <bernie.wangbj@gmail.com>
2013-09-04 18:20:24 +0800

    add prependchild test case

--- test/core.js
+++ test/core.js
@@ -1,4 +1,9 @@
-module( 'Core Dom' );
+module( 'Core Dom', {
+	setup: function () {
+		this.htmlElements = 'a abbr address area base blockquote button cite code dfn div fieldset form h1 h2 h3 h4 h5 h6 hr img input label object ol p pre select span table textarea ul';
+		this.html5Elements = 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video';
+	}
+} );
 
 test( 'add class test', function () {
 	var f = fore( 'qunit-fixture' );
@@ -38,4 +43,29 @@ test( 'remove class test', function () {
 	f.removeClass( 'double-a-class  double-d-class' );
 	deepEqual( fe.className, 'double-b-class double-e-class' );
 
+} );
+
+test( 'prepend child', function () {
+	var f = fore( 'qunit-fixture' );
+	var fe = f.getHtmlElements()[ 0 ];
+	var htmlElementsArray = this.htmlElements.split( ' ' );
+	var len = htmlElementsArray.length;
+	var prependElementTagNames = '';
+
+	for ( var i = len -1; i >= 0; i-- ) {
+		f.prependChild( document.createElement( htmlElementsArray[ i ] ) );
+	}
+
+	var childNodes = fe.childNodes;
+	var clen = childNodes.length;
+	for ( var j = 0; j < clen; j++ ) {
+		if ( j === 0 ) {
+			prependElementTagNames += childNodes[ j ].tagName.toLowerCase();
+		} else {
+			prependElementTagNames += ' ' + childNodes[ j ].tagName.toLowerCase();
+		}
+		
+	}
+
+	equal( prependElementTagNames, this.htmlElements );
 } );
