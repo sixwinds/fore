15e2270b21f5ce310d033abbb5cad7a5cad98a9c
Bernie <bernie.wangbj@gmail.com>
2013-09-17 17:36:00 +0800

    add event wrapper class: fore.Event

--- src/event.js
+++ src/event.js
@@ -1,37 +1,70 @@
 	var bindEvt;
 	var unbindEvt;
 
-	// 自定义event
-	// TODO
+	rootFore.Event = function ( e ) {
+		var fromElement = e.fromElement;
+
+		this.orignalEvent = e;
+		this.target = e.target || e.srcElement;
+		this.metaKey = !!e.metaKey;
+		this.relatedTarget = e.relatedTarget || ( fromElement === this.target ) ? e.fromElement : e.toElement;
+	};
+
+
+	rootFore.Event.prototype = {
+		stopPropagation: function () {
+			var oe = this.orignalEvent;
+
+			if ( oe.stopPropagation ) {
+				oe.stopPropagation();
+			}
+			oe.cancelBubble = true;
+		},
+
+		preventDefault: function () {
+			var oe = this.orignalEvent;
+
+			if ( oe.preventDefault ) {
+				oe.preventDefault();
+			}
+			oe.returnValue = false;
+		}
+	};
 
 	function toIeEventType( type ) {
 		return type ? 'on' + type : type;
+	}
+
+	function createListenerWrapper( listener ) {
+		return function ( e ) {
+			listener.apply( this, new rootFore.Event( e || window.event ) );
+		};
 	}
 
 	if ( document.addEventListener ) {
 		bindEvt = function ( el, type, listener, useCapture ) {
 			// 如果dom支持events模块，文档树中每个node都实现EventTarget接口
 			if ( el ) {
-				el.addEventListener( type, listener, useCapture );
+				el.addEventListener( type, createListenerWrapper( listener ), useCapture );
 			}
 		};
 
 		unbindEvt = function ( el, type, listener, useCapture ) {
 			if ( el ) {
-				el.removeEventListener( type, listener, useCapture );
+				el.removeEventListener( type, createListenerWrapper( listener ), useCapture );
 			}
 		};
 	} else {
 
 		bindEvt = function ( el, type, listener ) {
 			if ( el ) {
-				el.attachEvent( toIeEventType( type ), listener );
+				el.attachEvent( toIeEventType( type ), createListenerWrapper( listener ) );
 			}
 		};
 
 		unbindEvt = function ( el, type, listener ) {
 			if ( el ) {
-				el.detachEvent( toIeEventType( type ), listener );
+				el.detachEvent( toIeEventType( type ), createListenerWrapper( listener ) );
 			}
 		}
 	}
